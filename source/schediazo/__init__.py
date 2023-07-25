751aa2014e01463e64d37aec2e31b3a1b183dcb0
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2023-07-25 11:50:49 +0100

    Update __init__.py

--- source/schediazo/__init__.py
+++ source/schediazo/__init__.py
@@ -5,19 +5,17 @@
 ## ==========================================================================
 __author__ = "Chris Arridge"
 __copyright__ = "Copyright 2018-2023 Chris Arridge"
-__version__ = "0.2.0"
 __email__ = "chris@chrisarridge.co.uk"
 __status__ = "development"
-__date__ = " 17 June 2023"
 __docformat__ = "reStructuredText"
-
+from ._version import __version__, __version_tuple__
 
 from .drawing import Drawing
 from .shapes import Line, Circle, Ellipse, Rect, Polyline, Polygon, EquilateralTriangle
 from .text import Text, TextPath
 from .image import Image
 from .containers import Group, ClipPath
-from .paths import Path, RawPath, MoveTo,MoveToDelta,LineTo,LineToDelta,Line,HlineTo,HlineToDelta,Hline,VlineTo,VlineToDelta,Vline,CubicBezierTo,CubicBezierToDelta,QuadraticBezierTo,QuadraticBezierToDelta
+from .paths import Path, RawPath, MoveTo,MoveToDelta,LineTo,LineToDelta,PathLine,HlineTo,HlineToDelta,Hline,VlineTo,VlineToDelta,Vline,CubicBezierTo,CubicBezierToDelta,QuadraticBezierTo,QuadraticBezierToDelta
 from .units import mm, cm, m, inch
 from .transforms import Affine, identity
 from .svg import Versions
