2ad30021c7a73cc2896a5d971fd0ab742d8e748e
Chris Arridge <48034138+chrisarridge@users.noreply.github.com>
2023-08-09 15:51:43 +0100

    Changed units to use Pint

--- source/schediazo/units.py
+++ source/schediazo/units.py
@@ -1,24 +1,11 @@
 """Unit conversions from various dimensions"""
+from typing import Union, List
 
-# class UnitConversion:
-# 	def __init__(self, m):
-# 		self._scale = m*72/25.4e-3
+import numpy as np
+import pint
 
-# 	def __mul__(self, v):
-# 		return v*self._scale
+ureg = pint.UnitRegistry(filename=None)
 
-# 	def __rmul__(self, v):
-# 		return v*self._scale
-
-# """Default conversions that people will probably want to use most often"""
-# mm = UnitConversion(1e-3)
-# inch = UnitConversion(25.4e-3)
-# cm = UnitConversion(1e-2)
-# m = UnitConversion(1.0)
-
-
-from pint import UnitRegistry
-ureg = UnitRegistry(filename=None)
 
 # Prefixes.
 ureg.define("micro- = 1e-6  = µ- = μ- = u-")
@@ -30,8 +17,9 @@ ureg.define("hecto- = 1e2   = h-")
 ureg.define("kilo- =  1e3   = k-")
 
 # Basic lengths.
+ureg.define("px = [display_length]")
 ureg.define("meter = [length] = m = metre")
-ureg.define("pixel = [length] = px = pix")
+ureg.define("percent = [] = %")
 
 # Imperial lengths.
 ureg.define("inch = 25.4 * mm = in")
@@ -45,7 +33,34 @@ Q_ = ureg.Quantity
 
 cm = ureg.cm
 mm = ureg.mm
-m = ureg.m
 inch = ureg.inch
 pt = ureg.pt
 pc = ureg.pc
+
+def _tostr(q: pint.Quantity) -> str:
+    """Convert a length-type or % quantity to a string suitable for SVG.
+
+    Parameters
+    ----------
+    q : pint.Quantity
+        Quantity that should have dimensions of [length], [display_length] or be a percentage.
+
+    Returns
+    -------
+    str
+        Formatted string.
+
+    Raises
+    ------
+    ValueError
+        If the quantity doesn't have the right units/dimensions, e.g., a time.
+    """
+    if q.check("[length]"):
+        return ("{:f~P}".format(q.to(ureg.mm))).replace(" ","")
+    if q.check("[display_length]"):
+        return ("{:f~P}".format(q.to(ureg.px))).replace(" ","")
+    elif q.check(ureg.percent):
+        return ("{:f~P}".format(q.to(ureg.percent))).replace(" ","")
+    else:
+        raise ValueError("Cannot convert these units <{}> to a string for SVG".format(q.units))
+
